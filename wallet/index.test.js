57cc9857677586398062573b7f1d5fcaf0f39398
surendra dura <suren.dura@yahoo.com>
2021-12-10 17:42:37 +0000

    jjk

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,46 +1,99 @@
 const Wallet = require('./index');
+const Transaction = require('./transaction');
 const { verifySignature } = require('../cryptohash');
 const Blockchain = require('../blockchain');
 const { STARTING_BALANCE } = require('../config');
 
 describe('Wallet', () => {
-    let wallet;
+  let wallet;
   
-    beforeEach(() => {
-      wallet = new Wallet();
+  beforeEach(() => {
+    wallet = new Wallet();
+  });
+  
+  it('has a `balance`', () => {
+    expect(wallet).toHaveProperty('balance');
+  });
+  
+  it('has a `publicKey`', () => {
+    //console.log(wallet.publicKey);
+    expect(wallet).toHaveProperty('publicKey');
+  });
+
+  //checking the siging keyss
+  describe('signing data', () => {
+    const data = 'foobar';
+    
+    it('verifies a signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data)
+        })
+      ).toBe(true);
     });
-  
-    it('has a `balance`', () => {
-      expect(wallet).toHaveProperty('balance');
+    
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data)
+        })
+      ).toBe(false);
     });
-  
-    it('has a `publicKey`', () => {
-      //console.log(wallet.publicKey);
-      expect(wallet).toHaveProperty('publicKey');
+  });
+
+  // creating the Transaction in the wallet
+  describe('createTransaction()', () => {
+    describe('and the amount exceeds the balance', () => {
+      it('throws an error', () => {
+        expect(() => wallet.createTransaction({ amount: 999999, recipient: 'foo-recipient' }))
+          .toThrow('Amount exceeds balance');
+      });
     });
 
-    //checking the siging keyss
-    describe('signing data', () => {
-        const data = 'foobar';
-    
-        it('verifies a signature', () => {
-          expect(
-            verifySignature({
-              publicKey: wallet.publicKey,
-              data,
-              signature: wallet.sign(data)
-            })
-          ).toBe(true);
-        });
-    
-        it('does not verify an invalid signature', () => {
-          expect(
-            verifySignature({
-              publicKey: wallet.publicKey,
-              data,
-              signature: new Wallet().sign(data)
-            })
-          ).toBe(false);
-        });
+    describe('and the amount is valid', () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        recipient = 'foo-recipient';
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+
+      it('creates an instance of `Transaction`', () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+
+      it('matches the transaction input with the wallet', () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it('outputs the amount the recipient', () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
+
+    describe('and a chain is passed', () => {
+      it('calls `Wallet.calculateBalance`', () => {
+        const calculateBalanceMock = jest.fn();
+
+        const originalCalculateBalance = Wallet.calculateBalance;
+
+        Wallet.calculateBalance = calculateBalanceMock;
+
+        wallet.createTransaction({
+          recipient: 'foo',
+          amount: 10,
+          chain: new Blockchain().chain
+        });
+
+        expect(calculateBalanceMock).toHaveBeenCalled();
+
+        Wallet.calculateBalance = originalCalculateBalance;
+      });
+    });
+  });  
 });  
